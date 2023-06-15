c52b68a281d7e1c81417810536c5cbaef5ff2eff
Daviles82 <avileshouston@live.com>
2023-06-14 20:25:51 -0500

    second week assignment

---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Job
+{
+  public string _jobTitle;
+  public string _company;
+  public int _startYear;
+  public int _endYear;
+
+  public void Display()
+  {
+    Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+  }
+
+}
+
+
+
+
+
+// Job
+
+// _company: string
+
+// _jobTitle: string
+
+// _startYear: int,
+
+// _endYear : int,
+
+
+// Display() : void,
